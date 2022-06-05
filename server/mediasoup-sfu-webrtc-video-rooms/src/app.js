4faaaaffaee9ad2174b80039269a3e44c8f985d5
kj6560 <kj6560@gmail.com>
2022-06-05 21:24:52 +0530

    server modified

--- server/mediasoup-sfu-webrtc-video-rooms/src/app.js
+++ server/mediasoup-sfu-webrtc-video-rooms/src/app.js
@@ -18,8 +18,26 @@ const options = {
 const httpsServer = https.createServer(options, app)
 const io = require('socket.io')(httpsServer)
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://talktocounsel.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.use(express.static(path.join(__dirname, '..', 'public')))
 httpsServer.listen(config.listenPort, () => {
   console.log('Listening on https://' + config.listenIp + ':' + config.listenPort)
 })
